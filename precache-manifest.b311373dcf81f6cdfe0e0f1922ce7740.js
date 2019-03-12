b48832bcda227df57378f56f3ef11d0a90f4c116
Illia <pelypenkoillya@gmail.com>
2019-03-13 01:51:27 +0200

    Updates

---
+++ precache-manifest.b311373dcf81f6cdfe0e0f1922ce7740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7636ba80cf088e51f8d5",
+    "url": "/illiapelypenko/reactpracticetodolist/static/js/runtime~main.7563d749.js"
+  },
+  {
+    "revision": "08879b6c1f6cca7f17b3",
+    "url": "/illiapelypenko/reactpracticetodolist/static/js/main.305460b1.chunk.js"
+  },
+  {
+    "revision": "3f6648431bc23b783ffa",
+    "url": "/illiapelypenko/reactpracticetodolist/static/js/2.aad0938f.chunk.js"
+  },
+  {
+    "revision": "08879b6c1f6cca7f17b3",
+    "url": "/illiapelypenko/reactpracticetodolist/static/css/main.6fc583ea.chunk.css"
+  },
+  {
+    "revision": "38cca948073f57ecd54b75ce46081d80",
+    "url": "/illiapelypenko/reactpracticetodolist/index.html"
+  }
+];
